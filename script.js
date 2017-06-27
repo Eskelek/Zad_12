5f543f3bc2a673212b35740d844a32e1260dcb02
Eskel <lorthan966@wp.pl>
2017-06-27 11:10:14 +0200

    fin

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
 var url = 'https://restcountries.eu/rest/v2/name/';
-var cauntriesList = $('#countries');
+var countriesList = $('#countries');
 
 $('#search').click(searchCountries);
     
 function searchCountries() {
+    'use strict';
     var countryName = $('#country-name').val();
     
     if (!countryName.length) {
@@ -14,14 +15,19 @@ function searchCountries() {
     $.ajax({
         url: url + countryName,
         method: 'GET',
-        success: showCountriesList
+        success: showCountriesList,
+        error: function(){
+            alert('Sorry there is no country here');
+    }
     });
 }
         
 function showCountriesList(resp) {
+    'use strict';
     countriesList.empty();
     resp.forEach(function (item) {
-        $('<li>').text(item.name).appendTo(countriesList);
+        $('<h2>').text(item.name).appendTo(countriesList);
+        $('<p>').text("Capital City: " + item.capital).appendTo(countriesList);
     });
 }
 
