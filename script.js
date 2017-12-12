b2999b76270991373024cc7614a0dbb685a737f8
Eskel <lorthan966@wp.pl>
2017-12-12 20:31:59 +0100

    finally

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-
 var url = 'https://restcountries.eu/rest/v2/name/';
+
 var countriesList = $('#countries');
 
-$('#search').click(searchCountries);
+$("#menu").click(function () {
     
+    var menuItems = $(".menuItems");
+    
+    if (menuItems.hasClass("active")) {
+        menuItems.css({display: "none"}).removeClass("active");
+    } else {
+        menuItems.css({display: "block"}).addClass("active");
+    }
+});
+
+$('#search').click(searchCountries);
+
 function searchCountries() {
     'use strict';
     var countryName = $('#country-name').val();
@@ -16,9 +27,9 @@ function searchCountries() {
         url: url + countryName,
         method: 'GET',
         success: showCountriesList,
-        error: function(){
+        error: function () {
             alert('Sorry there is no country here');
-    }
+        }
     });
 }
         
@@ -28,7 +39,47 @@ function showCountriesList(resp) {
     resp.forEach(function (item) {
         $('<h2>').text(item.name).appendTo(countriesList);
         $('<p>').text("Capital City: " + item.capital).appendTo(countriesList);
+        $('<p>').text("Alternative : " + item.altSpellings).appendTo(countriesList);
+        $('<p>').text("Region: " + item.region).appendTo(countriesList);
+        $('<p>').text("Population: " + item.population).appendTo(countriesList);
+        $('<p>').text("Area: " + item.area).appendTo(countriesList);
+        $('<p>').text("Time zone: " + item.timezones).appendTo(countriesList);
     });
+}
+
+$('#country-name').keydown(function (e) {
+    $(window).alert("kkk");
+    var key = e.keyCode();
+    $(document).alert(key);
+    /*if (e.keyCode == "13" ) {
+        document.alert("kkk");
+    }
+    else {
+        return true
+    }*/
+});
+
+$(".fa-caret-right").click(moveOn);
+
+function moveOn() {
+    var windowW = window.innerWidth;
+    var find = $(".find");
+    var moveW = find.width();
+    var search = $("#search");
+    var countryName = $("#country-name");
+        
+    if (moveW > windowW * 0.5) {
+        find.css({width: "17%"});
+        search.css({display: "none"});
+        countryName.css({display: "none"});
+        $(".moveArrow").removeClass("fa-caret-left").addClass("fa-caret-right");
+    } else {
+        find.css({filter: "grayscale(40%)"});
+        find.css({width: "90%"});
+        search.css({display: "inline"});
+        countryName.css({display: "inline"});
+        $(".moveArrow").removeClass("fa-caret-right").addClass("fa-caret-left");
+    }
 }
 
     
